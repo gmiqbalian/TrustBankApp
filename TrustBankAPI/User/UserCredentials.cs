a2427b5e8578b4c2740ed9f5ae2cd3ec661f63d4
gmiqbalian <gmiqbalian@gmail.com>
2023-05-02 22:24:05 +0200

    JWT Token Authorization

---
+++ TrustBankAPI/User/UserCredentials.cs
@@ -0,0 +1,27 @@
+﻿namespace TrustBankAPI.User
+{
+    public class UserCredentials
+    {
+        public static List<UserModel> Users { get; set; } = new List<UserModel>()
+        {
+            new UserModel
+            {
+                GivenName = "ghazanfar",
+                SurName = "mahmood",
+                UserName = "gm-admin",
+                Password = "password",
+                EmailAddress = "email",
+                Role = "Admin"
+            },
+            new UserModel
+            {
+                GivenName = "ghazanfar",
+                SurName = "mahmood",
+                UserName = "gm-user",
+                Password = "password",
+                EmailAddress = "email",
+                Role = "User"
+            }
+        };
+    }
+}
