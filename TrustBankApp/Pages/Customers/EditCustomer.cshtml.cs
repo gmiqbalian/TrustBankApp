b2756fee7868ec7ff8d50bdbda77e6a08cf3fe12
gmiqbalian <gmiqbalian@gmail.com>
2023-05-07 07:00:06 +0200

    Edit Customer Page Styling Fix

--- TrustBankApp/Pages/Customers/EditCustomer.cshtml.cs
+++ TrustBankApp/Pages/Customers/EditCustomer.cshtml.cs
@@ -21,25 +21,28 @@ namespace TrustBankApp.Pages.Customers
             _customerService = customerService;
             _mapper = mapper;
         }
-        public EditCustomerViewModel EditCustomerViewModel { get; set; } = new EditCustomerViewModel();
+        public EditCustomerViewModel EditCustomerVM { get; set; } = new EditCustomerViewModel();
 
         public void OnGet(int customerId)
         {
-            EditCustomerViewModel.GendersDropDownList = _customerService.FillGenderDropDownList();
-            EditCustomerViewModel.CountriesDropDownList = _customerService.FillCountryDropDownList();
+            EditCustomerVM.GendersDropDownList = _customerService.FillGenderDropDownList();
+            EditCustomerVM.CountriesDropDownList = _customerService.FillCountryDropDownList();
 
             var customerToEdit = _customerService.GetCustomerById(customerId);
 
-            _mapper.Map(customerToEdit, EditCustomerViewModel);
+            _mapper.Map(customerToEdit, EditCustomerVM);
         }
         public IActionResult OnPost(int customerId)
         {
-            if(ModelState.IsValid)
-            {
-                EditCustomerViewModel.CustomerId = customerId;
-                _customerService.EditCustomer(EditCustomerViewModel);
+            EditCustomerVM.GendersDropDownList = _customerService.FillGenderDropDownList();
+            EditCustomerVM.CountriesDropDownList = _customerService.FillCountryDropDownList();
 
-                return RedirectToPage("/Customers/Customers");
+            if (ModelState.IsValid)
+            {
+                EditCustomerVM.CustomerId = customerId;
+                _customerService.EditCustomer(EditCustomerVM);
+
+                return RedirectToPage("/Customers/Customer", new { customerId = customerId });
             }
 
             return Page();
