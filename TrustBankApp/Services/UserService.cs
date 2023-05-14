d489ca720543f9b5ada97ebdbd42cdade8ff4999
gmiqbalian <gmiqbalian@gmail.com>
2023-05-14 21:53:22 +0200

    Final Version

--- TrustBankApp/Services/UserService.cs
+++ TrustBankApp/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using TrustBankApp.Infrastructure.Pagination;
 using TrustBankApp.Models;
 using TrustBankApp.Pages.Users;
